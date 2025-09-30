cc75026d33561a02dae1e75453312688152138fa
ephraim <ephraims.818@gmail.com>
2025-10-01 04:51:39 +0600

    REACT-COMPLETE-GUIDE--01-ESSENTIALS - 04. Making Components Reusable with Props

--- 01. Essentials/components-jsx-state-props/src/App.jsx
+++ 01. Essentials/components-jsx-state-props/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -21,11 +22,35 @@ function Header() {
     );
 }
 
+function CoreConcept(props) {
+    return (
+        <li>
+            <img src={props.image} alt={props.title} />
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+        </li>
+    );
+}
+
 function App() {
     return (
         <div>
             <Header />
+            <Header />
             <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept
+                            title="Components"
+                            description="The Core UI Building blocks"
+                            image={componentsImg}
+                        />
+                        <CoreConcept />
+                        <CoreConcept />
+                        <CoreConcept />
+                    </ul>
+                </section>
                 <h2>Time to get started!</h2>
             </main>
         </div>
