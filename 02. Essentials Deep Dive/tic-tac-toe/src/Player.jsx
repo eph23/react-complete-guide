4c08b5ba4a52c23ee3a9c375d322098cb81a966b
ephraim <ephraims.818@gmail.com>
2025-10-06 20:28:13 +0600

    REACT-COMPLETE-GUIDE--02-DEEP-DIVE--[TIC-TAC-TOE] - 04. Working With State

--- 02. Essentials Deep Dive/tic-tac-toe/src/Player.jsx
+++ 02. Essentials Deep Dive/tic-tac-toe/src/Player.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+import { defineConfig } from "vite";
+
 function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing(true);
+    }
+
+    let playerName = <span className="player-name">{name}</span>;
+
+    if (isEditing) {
+        playerName = <input type="text" required />;
+    }
+
     return (
         <li>
-            <span>
-                <span className="player-name">{name}</span>
+            <span className="player">
+                {playerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>Edit</button>
         </li>
     );
 }
