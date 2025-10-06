38aa45109445ebf16cd6c0be7969c2ae2c27394f
ephraim <ephraims.818@gmail.com>
2025-10-06 20:10:06 +0600

    REACT-COMPLETE-GUIDE--02-DEEP-DIVE--[TIC-TAC-TOE] - 02. First Steps Towards Our Game

--- 02. Essentials Deep Dive/tic-tac-toe/src/App.jsx
+++ 02. Essentials Deep Dive/tic-tac-toe/src/App.jsx
@@ -1,10 +1,23 @@
-
 function App() {
-  
+    return (
+        <main>
+            <div id="game-container">
+                <ol id="players">
+                    <li>
+                        <span className="player-name">Player 1</span>
+                        <span className="player-symbol">X</span>
+                    </li>
+                    <li>
+                        <span className="player-name">Player 1</span>
+                        <span className="player-symbol">O</span>
+                    </li>
+                </ol>
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+                <p>Game Board</p>
+            </div>
+            <p>Game Log</p>
+        </main>
+    );
 }
 
-export default App
+export default App;
