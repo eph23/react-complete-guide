b26fdf829b2967d168e6aa65d50d582b3d565a53
ephraim <ephraims.818@gmail.com>
2025-10-09 05:50:52 +0600

    REACT-COMPLETE-GUIDE--02-DEEP-DIVE--[TIC-TAC-TOE] - 16. Diabling Buttons Conditionally

--- 02. Essentials Deep Dive/tic-tac-toe/src/GameBoard.jsx
+++ 02. Essentials Deep Dive/tic-tac-toe/src/GameBoard.jsx
@@ -37,6 +37,7 @@ export default function GameBoard({ onSelectSquare, turns }) {
                                     onClick={() =>
                                         onSelectSquare(rowIndex, colIndex)
                                     }
+                                    disabled={playerSymbol !== null}
                                 >
                                     {playerSymbol}
                                 </button>
