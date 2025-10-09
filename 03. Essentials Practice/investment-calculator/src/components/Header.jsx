1510b3aa8dbed7148c5b9cec9d81c8d1ae9504f3
ephraim <ephraims.818@gmail.com>
2025-10-10 03:33:16 +0600

    REACT-COMPLETE-GUIDE--03-PRACTICE--[INVESTMENT-CALCULATOR] - 01. Adding Header Component

---
+++ 03. Essentials Practice/investment-calculator/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from "../assets/investment-calculator-logo.png";
+
+function Header() {
+    return (
+        <div id="header">
+            <img src={logo} alt="" />
+            <h1>Investment Calculator</h1>
+        </div>
+    );
+}
+
+export default Header;
