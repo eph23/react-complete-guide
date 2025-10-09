2020440a7e2edfb928f713928ad301762fa5ae48
ephraim <ephraims.818@gmail.com>
2025-10-10 04:41:49 +0600

    REACT-COMPLETE-GUIDE--03-PRACTICE--[INVESTMENT-CALCULATOR] - 05. Computing Values

--- 03. Essentials Practice/investment-calculator/src/components/Results.jsx
+++ 03. Essentials Practice/investment-calculator/src/components/Results.jsx
@@ -1,4 +1,8 @@
+import { calculateInvestmentResults } from "../util/investment";
+
 function Results({ input }) {
+    const resultsData = calculateInvestmentResults(input);
+    console.log(resultsData);
     return <p>Results</p>;
 }
 
