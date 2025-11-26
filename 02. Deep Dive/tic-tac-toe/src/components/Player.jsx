b6f9e168dddceac6de0e885bfd4c990a5f3b7453
ephraim <ephraims.818@gmail.com>
2025-11-26 16:15:11 +0600

    REACT-RCG - Deep Dive - [PROJECT] - TIC-TAC-TOE - 07. User Input and Two Way Binding

--- 02. Deep Dive/tic-tac-toe/src/components/Player.jsx
+++ 02. Deep Dive/tic-tac-toe/src/components/Player.jsx
@@ -1,24 +1,36 @@
 import { useState } from "react";
 
-function Player({ name, symbol }) {
+function Player({ initialName, symbol }) {
     const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
 
     function handleEditClick() {
         setIsEditing((editing) => !editing);
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
     let btnCaption = "Edit";
 
     if (isEditing) {
-        playerName = <input type="text" required value={name} />;
+        editablePlayerName = (
+            <input
+                type="text"
+                required
+                value={playerName}
+                onChange={handleChange}
+            />
+        );
         btnCaption = "Save";
     }
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{btnCaption}</button>
