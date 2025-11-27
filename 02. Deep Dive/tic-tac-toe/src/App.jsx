337f43bdff742e96bce2b0e2779184bbaf930a0a
ephraim <ephraims.818@gmail.com>
2025-11-27 13:39:55 +0600

    REACT-RCG - Deep Dive - [PROJECT] - TIC-TAC-TOE - 10. Lifting State Up

--- 02. Deep Dive/tic-tac-toe/src/App.jsx
+++ 02. Deep Dive/tic-tac-toe/src/App.jsx
@@ -1,15 +1,36 @@
+import { useState } from "react";
+
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 
 function App() {
+    const [activePlayer, setActivePlayer] = useState("X");
+
+    function handleSelectSquare() {
+        setActivePlayer((curActivePlayer) =>
+            curActivePlayer === "X" ? "O" : "X"
+        );
+    }
+
     return (
         <main>
             <div id="game-container">
-                <ol id="players">
-                    <Player initialName="player 1" symbol="X" />
-                    <Player initialName="player 2" symbol="O " />
+                <ol id="players" className="highlight-player">
+                    <Player
+                        initialName="player 1"
+                        symbol="X"
+                        isActive={activePlayer === "X"}
+                    />
+                    <Player
+                        initialName="player 2"
+                        symbol="O "
+                        isActive={activePlayer === "O"}
+                    />
                 </ol>
-                <GameBoard />
+                <GameBoard
+                    onSelectSquare={handleSelectSquare}
+                    activePlayerSymbol={activePlayer}
+                />
             </div>
         </main>
     );
