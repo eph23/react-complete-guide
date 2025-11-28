c3c66e5e6997d3a848ea9feb6687156c3ce3d6c9
ephraim <ephraims.818@gmail.com>
2025-11-28 18:02:22 +0600

    REACT-RCG - Deep Dive - [PROJECT] - TIC-TAC-TOE - 13. Deriving State From Props

--- 02. Deep Dive/tic-tac-toe/src/App.jsx
+++ 02. Deep Dive/tic-tac-toe/src/App.jsx
@@ -1,29 +1,28 @@
 import { useState } from "react";
 
-import Player from "./components/Player";
-import GameBoard from "./components/GameBoard";
-import Log from "./components/Log";
+import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
+import Log from "./components/Log.jsx";
 
 function App() {
-    const [activePlayer, setActivePlayer] = useState("X");
     const [gameTurns, setGameTurns] = useState([]);
+    const [activePlayer, setActivePlayer] = useState("X");
 
-    function handleSelectSquare(roeIndex, colIndex) {
+    function handleSelectSquare(rowIndex, colIndex) {
         setActivePlayer((curActivePlayer) =>
             curActivePlayer === "X" ? "O" : "X"
         );
-
-        let currentPlayer = "X";
-
-        if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-            currentPlayer = "O";
-        }
-
         setGameTurns((prevTurns) => {
+            let currentPlayer = "X";
+
+            if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+                currentPlayer = "O";
+            }
+
             const updatedTurns = [
                 {
-                    square: { row: roeIndex, col: colIndex },
-                    Player: activePlayer,
+                    square: { row: rowIndex, col: colIndex },
+                    player: currentPlayer,
                 },
                 ...prevTurns,
             ];
@@ -37,22 +36,22 @@ function App() {
             <div id="game-container">
                 <ol id="players" className="highlight-player">
                     <Player
-                        initialName="player 1"
+                        initialName="Player 1"
                         symbol="X"
                         isActive={activePlayer === "X"}
                     />
                     <Player
-                        initialName="player 2"
-                        symbol="O "
+                        initialName="Player 2"
+                        symbol="O"
                         isActive={activePlayer === "O"}
                     />
                 </ol>
                 <GameBoard
                     onSelectSquare={handleSelectSquare}
-                    activePlayerSymbol={activePlayer}
+                    turns={gameTurns}
                 />
-                <Log />
             </div>
+            <Log />
         </main>
     );
 }
--- 02. Deep Dive/tic-tac-toe/src/components/GameBoard.jsx
+++ 02. Deep Dive/tic-tac-toe/src/components/GameBoard.jsx
@@ -1,11 +1,31 @@
-
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ];
 
-function GameBoard({ onSelectSquare }) {
+export default function GameBoard({ onSelectSquare, turns }) {
+    let gameBoard = initialGameBoard;
+
+    for (const turn of turns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+
+        gameBoard[row][col] = player;
+    }
+
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    // function handleSelectSquare(rowIndex, colIndex) {
+    //   setGameBoard((prevGameBoard) => {
+    //     const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+    //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //     return updatedBoard;
+    //   });
+
+    //   onSelectSquare();
+    // }
+
     return (
         <ol id="game-board">
             {gameBoard.map((row, rowIndex) => (
@@ -13,7 +33,11 @@ function GameBoard({ onSelectSquare }) {
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                             <li key={colIndex}>
-                                <button onClick={onSelectSquare}>
+                                <button
+                                    onClick={() =>
+                                        onSelectSquare(rowIndex, colIndex)
+                                    }
+                                >
                                     {playerSymbol}
                                 </button>
                             </li>
@@ -24,5 +48,3 @@ function GameBoard({ onSelectSquare }) {
         </ol>
     );
 }
-
-export default GameBoard;
