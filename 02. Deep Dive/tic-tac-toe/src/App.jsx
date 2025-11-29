abf316ce59b75a98e6c822f22be2c8bd86c4c8d4
ephraim <ephraims.818@gmail.com>
2025-11-30 04:09:26 +0600

    REACT-RCG - Deep Dive - [PROJECT] - TIC-TAC-TOE - 21. Why Immutability Matters

--- 02. Deep Dive/tic-tac-toe/src/App.jsx
+++ 02. Deep Dive/tic-tac-toe/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
     const activePlayer = deriveActivePlayer(gameTurns);
 
-    let gameBoard = initialGameBoard;
+    let gameBoard = [...initialGameBoard.map((array) => [...array])];
 
     for (const turn of gameTurns) {
         const { square, player } = turn;
@@ -76,6 +76,10 @@ function App() {
         });
     }
 
+    function handleRestart() {
+        setGameTurns([]);
+    }
+
     return (
         <main>
             <div id="game-container">
@@ -91,7 +95,9 @@ function App() {
                         isActive={activePlayer === "O"}
                     />
                 </ol>
-                {(winner || hasDraw) && <GameOver winner={winner} />}
+                {(winner || hasDraw) && (
+                    <GameOver winner={winner} onRestart={handleRestart} />
+                )}
                 <GameBoard
                     onSelectSquare={handleSelectSquare}
                     board={gameBoard}
