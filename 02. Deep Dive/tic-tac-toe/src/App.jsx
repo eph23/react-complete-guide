a1b4a7a3b4a729133ba506f989357a376c4bfc66
ephraim <ephraims.818@gmail.com>
2025-11-30 03:59:19 +0600

    REACT-RCG - Deep Dive - [PROJECT] - TIC-TAC-TOE - 19. Deriving Computed Values From Other Computed Values

--- 02. Deep Dive/tic-tac-toe/src/App.jsx
+++ 02. Deep Dive/tic-tac-toe/src/App.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
-import GameOver from "./components/GameOver.jsx";
 import { WINNING_COMBINATIONS } from "./winning-combinations.js";
 
 const initialGameBoard = [
@@ -57,8 +56,6 @@ function App() {
         }
     }
 
-    const hasDraw = gameTurns.length === 9 && !winner;
-
     function handleSelectSquare(rowIndex, colIndex) {
         // setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
         setGameTurns((prevTurns) => {
@@ -91,7 +88,7 @@ function App() {
                         isActive={activePlayer === "O"}
                     />
                 </ol>
-                {(winner || hasDraw) && <GameOver winner={winner} />}
+                {winner && <p>You won, {winner}!</p>}
                 <GameBoard
                     onSelectSquare={handleSelectSquare}
                     board={gameBoard}
