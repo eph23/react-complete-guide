1a814efb9e89e6f425c4ed9fdb6fa1ce8d8ddb8e
ephraim <ephraims.818@gmail.com>
2025-12-04 02:39:19 +0600

    REACT-RCG - Styling React Components - 04. Dynamic and Conditional Styling with CSS Files and CSS Classes

--- 04. Styling Components/react-art/src/components/AuthInputs.jsx
+++ 04. Styling Components/react-art/src/components/AuthInputs.jsx
@@ -24,30 +24,38 @@ export default function AuthInputs() {
         <div id="auth-inputs">
             <div className="controls">
                 <p>
-                    <label>Email</label>
+                    <label
+                        className={`label ${emailNotValid ? "invalid" : ""}`}
+                    >
+                        Email
+                    </label>
                     <input
                         type="email"
-                        // className={emailNotValid ? 'invalid' : undefined}
-                        style={{
+                        className={emailNotValid ? "invalid" : undefined}
+                        /* style={{
                             backgroundColor: emailNotValid
                                 ? "#fed2d2"
                                 : "#d1d5db",
-                        }}
+                        }} */
                         onChange={(event) =>
                             handleInputChange("email", event.target.value)
                         }
                     />
                 </p>
                 <p>
-                    <label>Password</label>
+                    <label
+                        className={`label ${emailNotValid ? "invalid" : ""}`}
+                    >
+                        Password
+                    </label>
                     <input
                         type="password"
-                        // className={passwordNotValid ? 'invalid' : undefined}
-                        style={{
+                        className={passwordNotValid ? "invalid" : undefined}
+                        /* style={{
                             backgroundColor: emailNotValid
                                 ? "#fed2d2"
                                 : "#d1d5db",
-                        }}
+                        }} */
                         onChange={(event) =>
                             handleInputChange("password", event.target.value)
                         }
