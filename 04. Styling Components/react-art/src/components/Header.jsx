f2dacd069c750a107645ee64bdc201a3ec833bc2
ephraim <ephraims.818@gmail.com>
2025-12-06 15:31:50 +0600

    REACT-RCG - Styling React Components - 12. Adding and Using Tailwing CSS

--- 04. Styling Components/react-art/src/components/Header.jsx
+++ 04. Styling Components/react-art/src/components/Header.jsx
@@ -8,13 +8,12 @@ export default function Header() {
                 alt="A canvas"
                 className="mb-8 w-44 h-44 object-contain"
             />
-            <h1 className="text-4xl font-bold tracking-widest text-center uppercase text-amber-800">
+            <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
                 ReactArt
             </h1>
-            {/* <p className={`${classes.paragraph}`}>
+            <p className="text-stone-500">
                 A community of artists and art-lovers.
-            </p> */}
-            <p>A community of artists and art-lovers.</p>
+            </p>
         </header>
     );
 }
