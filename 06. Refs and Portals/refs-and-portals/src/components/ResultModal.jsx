536cfbbc48c479860b0c56c6a66f102d03db3619
ephraim <ephraims.818@gmail.com>
2025-12-08 15:51:30 +0600

    REACT-RCG - Refs and Portals - 12. Enhancing the Demo App Result Modal

--- 06. Refs and Portals/refs-and-portals/src/components/ResultModal.jsx
+++ 06. Refs and Portals/refs-and-portals/src/components/ResultModal.jsx
@@ -5,6 +5,7 @@ function ResultModal({ ref, targetTime, remainingTime, onReset }) {
 
     const userLost = remainingTime <= 0;
     const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
     useImperativeHandle(ref, () => {
         return {
@@ -17,8 +18,9 @@ function ResultModal({ ref, targetTime, remainingTime, onReset }) {
     return (
         <dialog ref={dialog} className="result-modal">
             {userLost && <h2>You lost</h2>}
+            {!userLost && <h2>Your Score: {score}</h2>}
             <p>
-                The target time was <strong>{targetTime}</strong>
+                The target time was <strong>{targetTime} seconds</strong>
             </p>
             <p>
                 You stopped the timer with{" "}
