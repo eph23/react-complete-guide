58165bc778c3a6695f8f29128a03340089445a54
ephraim <ephraims.818@gmail.com>
2025-12-08 15:42:36 +0600

    REACT-RCG - Refs and Portals - 11. Sharing State Across Components

--- 06. Refs and Portals/refs-and-portals/src/components/ResultModal.jsx
+++ 06. Refs and Portals/refs-and-portals/src/components/ResultModal.jsx
@@ -1,7 +1,10 @@
 import { useImperativeHandle, useRef } from "react";
 
-function ResultModal({ ref, result, targetTime }) {
+function ResultModal({ ref, targetTime, remainingTime, onReset }) {
     const dialog = useRef();
+
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
     useImperativeHandle(ref, () => {
         return {
@@ -13,14 +16,15 @@ function ResultModal({ ref, result, targetTime }) {
 
     return (
         <dialog ref={dialog} className="result-modal">
-            <h2>You {result}</h2>
+            {userLost && <h2>You lost</h2>}
             <p>
                 The target time was <strong>{targetTime}</strong>
             </p>
             <p>
-                You stopped the timer with <strong>X Seconds left</strong>
+                You stopped the timer with{" "}
+                <strong>{formattedRemainingTime} Seconds left</strong>
             </p>
-            <form method="dialog">
+            <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
