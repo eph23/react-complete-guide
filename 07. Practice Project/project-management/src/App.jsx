dad490592360d4ebcb882a6fb4ea19ce75756ec0
ephraim <ephraims.818@gmail.com>
2025-12-10 03:47:27 +0600

    REACT-RCG - Practice Project - [PROJECT] - PROJECT-MANAGEMENT-APP - 07. Collecting User Input with Refs and Forwarded Refs

--- 07. Practice Project/project-management/src/App.jsx
+++ 07. Practice Project/project-management/src/App.jsx
@@ -4,13 +4,13 @@ import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
-    const [projectState, setProjectState] = useState({
+    const [projectsState, setProjectsState] = useState({
         selectedProjectId: undefined,
         projects: [],
     });
 
     function handleStartAddProject() {
-        setProjectState((prevState) => {
+        setProjectsState((prevState) => {
             return {
                 ...prevState,
                 selectedProjectId: null,
@@ -18,10 +18,26 @@ function App() {
         });
     }
 
+    function handleAddProject(projectData) {
+        setProjectsState((prevState) => {
+            const newProject = {
+                ...projectData,
+                id: Math.random(),
+            };
+
+            return {
+                ...prevState,
+                projects: [...prevState.projects, newProject],
+            };
+        });
+    }
+
+    console.log(projectsState);
+
     let content;
-    if (projectState.selectedProjectId === null) {
-        content = <NewProject />;
-    } else if (projectState.selectedProjectId === undefined) {
+    if (projectsState.selectedProjectId === null) {
+        content = <NewProject onAdd={handleAddProject} />;
+    } else if (projectsState.selectedProjectId === undefined) {
         content = (
             <NoProjectSelected onStartAddProject={handleStartAddProject} />
         );
--- 07. Practice Project/project-management/src/components/NoProjectSelected.jsx
+++ 07. Practice Project/project-management/src/components/NoProjectSelected.jsx
@@ -1,7 +1,7 @@
 import noProjectImage from "../assets/no-projects.png";
 import Button from "./Button";
 
-function NoProjectSelected(onStartAddProject) {
+function NoProjectSelected({onStartAddProject}) {
     return (
         <div className="mt-24 text-center w-2/3">
             <img
@@ -16,7 +16,7 @@ function NoProjectSelected(onStartAddProject) {
                 Select a project or get started with a new one
             </p>
             <p className="mt-8">
-                <Button onCLick={onStartAddProject}>Create new project</Button>
+                <Button onClick={onStartAddProject}>Create new project</Button>
             </p>
         </div>
     );
