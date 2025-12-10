cad12c037f1d604a874eb295936652aa21db5aa9
ephraim <ephraims.818@gmail.com>
2025-12-11 02:33:39 +0600

    REACT-RCG - Practice Project - [PROJECT] - PROJECT-MANAGEMENT-APP - 15. Claring Tasks and Fixing Minor Bugs

--- 07. Practice Project/project-management/src/App.jsx
+++ 07. Practice Project/project-management/src/App.jsx
@@ -27,7 +27,14 @@ function App() {
         });
     }
 
-    function handleDeleteTask() {}
+    function handleDeleteTask(id) {
+        setProjectsState((prevState) => {
+            return {
+                ...prevState,
+                tasks: prevState.tasks.filter((task) => task.id !== id),
+            };
+        });
+    }
 
     function handleStartAddProject() {
         setProjectsState((prevState) => {
@@ -82,7 +89,7 @@ function App() {
             onDelete={handleDeleteProject}
             onAddTask={handleAddTask}
             onDeleteTask={handleDeleteTask}
-            tasks = {projectsState.tasks}
+            tasks={projectsState.tasks}
         />
     );
 
@@ -117,6 +124,7 @@ function App() {
                 onStartAddProject={handleStartAddProject}
                 projects={projectsState.projects}
                 onSelectProject={handleSelectProject}
+                selectedProjectId={projectsState.selectedProjectId}
             />
             {content}
         </main>
