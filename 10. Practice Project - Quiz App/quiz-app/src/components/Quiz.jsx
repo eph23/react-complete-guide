c7b3d65b643ad6b295f2173ccb378dfd36d19cc6
ephraim <ephraims.818@gmail.com>
2025-12-20 01:05:50 +0600

    REACT-RCG - Practice Project - [PROJECT] - QUIZ-APP - 02. Deriving Values, Outputting Questions and Registering Answers

--- 10. Practice Project - Quiz App/quiz-app/src/components/Quiz.jsx
+++ 10. Practice Project - Quiz App/quiz-app/src/components/Quiz.jsx
@@ -1,10 +1,38 @@
 import { useState } from "react";
 
+import QUESTIONS from "../questions";
+
 function Quiz() {
-    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
     const [userAnswers, setUserAnswers] = useState([]);
 
-    return <p>Currently ActiveQuestion</p>;
+    const activeQuestionIndex = userAnswers.length;
+
+    function handleSelectAnswer(selectedAnser) {
+        setUserAnswers((prevUserAnswers) => {
+            return [...prevUserAnswers, selectedAnser];
+        });
+    }
+
+    return (
+        <div id="quiz">
+            <div id="question">
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id="answers">
+                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                        <li key={answer} className="answer">
+                            {
+                                <button
+                                    onClick={() => handleSelectAnswer(answer)}
+                                >
+                                    {answer}
+                                </button>
+                            }
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default Quiz;
