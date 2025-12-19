c1c8f107ce6cb941f6846616a6dbe24ce0c2a3bc
ephraim <ephraims.818@gmail.com>
2025-12-20 01:17:18 +0600

    REACT-RCG - Practice Project - [PROJECT] - QUIZ-APP - 03. Shuffling Answers and Adding Quiz Logic

--- 10. Practice Project - Quiz App/quiz-app/src/components/Quiz.jsx
+++ 10. Practice Project - Quiz App/quiz-app/src/components/Quiz.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
+import quizCompleteImg from "../assets/quiz-complete.png";
+
 import QUESTIONS from "../questions";
 
 function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     function handleSelectAnswer(selectedAnser) {
         setUserAnswers((prevUserAnswers) => {
@@ -13,12 +16,23 @@ function Quiz() {
         });
     }
 
+    if (quizIsComplete) {
+        return (
+            <div id="summary">
+                <img src="quizCompleteImg" alt="Trophy Image" />
+                <h2>Quiz Completed!</h2>
+            </div>
+        );
+    }
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
     return (
         <div id="quiz">
             <div id="question">
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                    {shuffledAnswers.map((answer) => (
                         <li key={answer} className="answer">
                             {
                                 <button
