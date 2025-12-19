95708d27365d3436d7d727a61e733404c9232072
ephraim <ephraims.818@gmail.com>
2025-12-19 14:17:57 +0600

    REACT-RCG - Working With useEffect - 14. Optimizing State Updates

--- 09. Side Effects and Working with the useEffect/place-picker/src/components/DeleteConfirmation.jsx
+++ 09. Side Effects and Working with the useEffect/place-picker/src/components/DeleteConfirmation.jsx
@@ -1,29 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import ProgressBar from "./ProgressBar";
 
 const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-    const [remainingTime, setRemainingTime] = useState(TIMER);
-
     useEffect(() => {
-        const interval = setInterval(() => {
-            console.log("INTERVAL");
-            setRemainingTime((prevTime) => prevTime - 10);
-        }, 10);
-
-        return () => {
-            clearInterval(interval);
-        };
-    }, []);
-
-    useEffect(() => {
-        console.log("TIMER SET");
         const timer = setTimeout(() => {
             onConfirm();
         }, TIMER);
 
         return () => {
-            console.log("Cleaning up timer");
             clearTimeout(timer);
         };
     }, [onConfirm]);
@@ -40,7 +26,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
                     Yes
                 </button>
             </div>
-            <progress value={remainingTime} max={TIMER} />
+            <ProgressBar timer={TIMER} />
         </div>
     );
 }
