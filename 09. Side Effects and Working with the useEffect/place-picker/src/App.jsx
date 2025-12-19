82e34d8612ac5fa39276df696c0ae9cb2394bbe7
ephraim <ephraims.818@gmail.com>
2025-12-19 08:29:53 +0600

    REACT-RCG - Working With useEffect - 12. The useCallback Hook

--- 09. Side Effects and Working with the useEffect/place-picker/src/App.jsx
+++ 09. Side Effects and Working with the useEffect/place-picker/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, useCallback } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
@@ -59,13 +59,13 @@ function App() {
         }
     }
 
-    function handleRemovePlace() {
+    const handleRemovePlace = useCallback(function handleRemovePlace() {
         setPickedPlaces((prevPickedPlaces) =>
             prevPickedPlaces.filter(
                 (place) => place.id !== selectedPlace.current
             )
         );
-        // setModalIsOpen(false);
+        setModalIsOpen(false);
 
         const storedIds =
             JSON.parse(localStorage.getItem("selectedPlaces")) || [];
@@ -75,7 +75,7 @@ function App() {
                 storedIds.filter((id) => id !== selectedPlace.current)
             )
         );
-    }
+    }, []);
 
     return (
         <>
