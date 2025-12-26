a77472678cbb6f938a3475952b708e2f1a68c1b7
ephraim <ephraims.818@gmail.com>
2025-12-27 02:26:42 +0600

    REACT-RCG - Behind The Scenes of React - 08. Optimizing React with MillionJS

--- 11. Behind The Scenes/behind-the-scenes/src/App.jsx
+++ 11. Behind The Scenes/behind-the-scenes/src/App.jsx
@@ -21,7 +21,6 @@ function App() {
             <main>
                 <ConfigureCounter onSet={handleSetCount} />
                 <Counter key={chosenCount} initialCount={chosenCount} />
-                <Counter initialCount={0} />
             </main>
         </>
     );
