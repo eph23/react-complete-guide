a54ebe6da86cbd0ac3af5d10702ad4e8c16f0106
ephraim <ephraims.818@gmail.com>
2025-12-28 12:56:50 +0600

    REACT-RCG - HTTP Requests - 02. How Not to Send HTTP Requests

--- 12. HTTP Requests/place-picker/src/components/AvailablePlaces.jsx
+++ 12. HTTP Requests/place-picker/src/components/AvailablePlaces.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 
 import Places from "./Places.jsx";
- 
 
 export default function AvailablePlaces({ onSelectPlace }) {
     const [availablePlaces, setAvailablePlaces] = useState([]);
+
+    fetch("http://loaclhost:3000/places").then(response=> {
+      response.json()
+    }).then(resData=> {
+      setAvailablePlaces(resData.places)
+    });
 
     return (
         <Places
