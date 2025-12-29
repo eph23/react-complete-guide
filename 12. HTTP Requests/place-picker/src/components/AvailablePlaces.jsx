de35729225ef6e4d47625d045cac33c113180cbc
ephraim <ephraims.818@gmail.com>
2025-12-29 16:34:59 +0600

    REACT-RCG - HTTP Requests - 07. Transforming Fetched Data

--- 12. HTTP Requests/place-picker/src/components/AvailablePlaces.jsx
+++ 12. HTTP Requests/place-picker/src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 import Places from "./Places.jsx";
 import Error from "./Error";
+import { sortPlacesByDistance } from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
     const [isFetching, setIsFetching] = useState();
@@ -19,16 +20,24 @@ export default function AvailablePlaces({ onSelectPlace }) {
                 if (!response.ok) {
                     throw new Error("Failed to fetch places");
                 }
-                setAvailablePlaces(resData.places);
+
+                navigator.geolocation.getCurrentPosition((position) => {
+                    const sortedPlaces = sortPlacesByDistance(
+                        resData.places,
+                        position.coords.latitude,
+                        position.coords.longitude
+                    );
+                    setAvailablePlaces(sortedPlaces);
+                    setIsFetching(false);
+                });
             } catch (error) {
                 setError({
                     message:
                         error.message ||
                         "Could not fetch places, please try again",
                 });
+                setIsFetching(false);
             }
-
-            setIsFetching(false);
         }
 
         fetchPlaces();
