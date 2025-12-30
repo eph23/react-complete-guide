a21ba8ab32dfd8567cfaf44f82f95fe28bb478f2
ephraim <ephraims.818@gmail.com>
2025-12-31 02:14:07 +0600

    REACT-RCG - HTTP Requests - 11. Deleting Data

--- 12. HTTP Requests/place-picker/src/components/AvailablePlaces.jsx
+++ 12. HTTP Requests/place-picker/src/components/AvailablePlaces.jsx
@@ -1,56 +1,55 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from 'react';
 
-import Places from "./Places.jsx";
-import Error from "./Error";
-import { sortPlacesByDistance } from "../loc.js";
-import { fetchAvailablePlaces } from "../http.js";
+import Places from './Places.jsx';
+import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from '../http.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
-    const [isFetching, setIsFetching] = useState();
-    const [availablePlaces, setAvailablePlaces] = useState([]);
-    const [error, setError] = useState();
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
-    useEffect(() => {
-        async function fetchPlaces() {
-            setIsFetching(true);
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
 
-            try {
-                const places = await fetchAvailablePlaces();
+      try {
+        const places = await fetchAvailablePlaces();
 
-                navigator.geolocation.getCurrentPosition((position) => {
-                    const sortedPlaces = sortPlacesByDistance(
-                        places,
-                        position.coords.latitude,
-                        position.coords.longitude
-                    );
-                    setAvailablePlaces(sortedPlaces);
-                    setIsFetching(false);
-                });
-            } catch (error) {
-                setError({
-                    message:
-                        error.message ||
-                        "Could not fetch places, please try again",
-                });
-                setIsFetching(false);
-            }
-        }
-
-        fetchPlaces();
-    }, []);
-
-    if (error) {
-        return <Error title="An error occurred!" message={error.message} />;
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setError({
+          message:
+            error.message || 'Could not fetch places, please try again later.',
+        });
+        setIsFetching(false);
+      }
     }
 
-    return (
-        <Places
-            title="Available Places"
-            places={availablePlaces}
-            isLoading={isFetching}
-            loadingText="Fetching place data..."
-            fallbackText="No places available."
-            onSelectPlace={onSelectPlace}
-        />
-    );
+    fetchPlaces();
+  }, []);
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
 }
--- 12. HTTP Requests/place-picker/src/http.js
+++ 12. HTTP Requests/place-picker/src/http.js
@@ -21,7 +21,7 @@ export async function updateUserPlaces(places) {
     const resData = await response.json();
 
     if (!response.ok) {
-        throw new Error("Failed to update user data");
+        throw new Error("Failed to update user data.");
     }
 
     return resData.message;
