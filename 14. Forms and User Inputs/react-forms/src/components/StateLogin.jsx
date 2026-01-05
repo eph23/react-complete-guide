242ec6438401fee106b15e4112c2c72b7a9fab25
ephraim <ephraims.818@gmail.com>
2026-01-05 15:13:21 +0600

    REACT-RCG - Forms and User Input - 06. Validating Input on Every Keystroke via State

--- 14. Forms and User Inputs/react-forms/src/components/StateLogin.jsx
+++ 14. Forms and User Inputs/react-forms/src/components/StateLogin.jsx
@@ -9,6 +9,9 @@ export default function Login() {
         password: "",
     });
 
+    const emailIsInvalid =
+        enteredValues.email !== "" && !enteredValues.email.includes("@");
+
     function handleSubmit(event) {
         event.preventDefault();
         console.log(enteredValues);
@@ -38,6 +41,12 @@ export default function Login() {
                         }
                         value={enteredValues.email}
                     />
+
+                    <div className="control-error">
+                        {emailIsInvalid && (
+                            <p>Please enter a valid email address</p>
+                        )}
+                    </div>
                 </div>
 
                 <div className="control no-margin">
