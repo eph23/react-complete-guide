5be9e0559dfda4589a73ffda5d023162a9370ebd
ephraim <ephraims.818@gmail.com>
2026-01-09 18:06:33 +0600

    REACT-RCG - Form Actions - [PROJECT]  - 03. Updating the UI With useFormStatus

--- 15. Form Actions/form-action-project/src/components/NewOpinion.jsx
+++ 15. Form Actions/form-action-project/src/components/NewOpinion.jsx
@@ -1,5 +1,6 @@
 import { useActionState, use } from "react";
 import { OpinionsContext } from "../store/opinions-context";
+import Submit from "./Submit";
 
 export function NewOpinion() {
     const { addOpinion } = use(OpinionsContext);
@@ -84,9 +85,7 @@ export function NewOpinion() {
                     </ul>
                 )}
 
-                <p className="actions">
-                    <button type="submit">Submit</button>
-                </p>
+                <Submit />
             </form>
         </div>
     );
