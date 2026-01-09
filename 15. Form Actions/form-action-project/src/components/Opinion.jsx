1d14e479bbe8464db2daae1814e92858210f2d90
ephraim <ephraims.818@gmail.com>
2026-01-09 21:20:53 +0600

    REACT-RCG - Form Actions - [PROJECT]  - 06. Using the pending State from useActionState

--- 15. Form Actions/form-action-project/src/components/Opinion.jsx
+++ 15. Form Actions/form-action-project/src/components/Opinion.jsx
@@ -1,4 +1,4 @@
-import { use } from "react";
+import { use, useActionState } from "react";
 import { OpinionsContext } from "../store/opinions-context";
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
     const { upvoteOpinion, downvoteOpinion } = use(OpinionsContext);
@@ -10,6 +10,11 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
         await downvoteOpinion(id);
     }
 
+    const [upvoteFormState, upvoteFormAction, upvotePending] =
+        useActionState(upvoteAction);
+    const [downvoteFormState, downvoteFormAction, downvotePending] =
+        useActionState(downvoteAction);
+
     return (
         <article>
             <header>
@@ -18,7 +23,10 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
             </header>
             <p>{body}</p>
             <form className="votes">
-                <button formAction={upvoteAction}>
+                <button
+                    formAction={upvoteFormAction}
+                    disabled={upvotePending || downvotePending}
+                >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="24"
@@ -38,7 +46,10 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
 
                 <span>{votes}</span>
 
-                <button formAction={downvoteAction}>
+                <button
+                    formAction={downvoteFormAction}
+                    disabled={upvotePending || downvotePending}
+                >
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="24"
