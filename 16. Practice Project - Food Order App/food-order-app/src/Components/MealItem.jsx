6e915ed1a325f3d9fd88a078bc9f42e03445ee86
ephraim <ephraims.818@gmail.com>
2026-01-10 18:49:35 +0600

    REACT-RCG - Practice Project - [PROJECT] - FOOD-ORDER-APP  - 04. Formatting and Outputting Numbers as Currency

--- 16. Practice Project - Food Order App/food-order-app/src/Components/MealItem.jsx
+++ 16. Practice Project - Food Order App/food-order-app/src/Components/MealItem.jsx
@@ -1,3 +1,5 @@
+import currencyFormatter from "../util/Formatting";
+
 function MealItem({ meal }) {
     return (
         <li className="meal-item">
@@ -8,8 +10,10 @@ function MealItem({ meal }) {
                 />
                 <div>
                     <h3>{meal.name}</h3>
-                    <p className="meal-tem-price">{meal.price}</p>
-                    <p className="meal-item-price">{meal.description}</p>
+                    <p className="meal-item-price">
+                        {currencyFormatter.format(meal.price)}
+                    </p>
+                    <p className="meal-item-description">{meal.description}</p>
                 </div>
                 <p className="meal-item-actions">
                     <button>Add to Cart</button>
