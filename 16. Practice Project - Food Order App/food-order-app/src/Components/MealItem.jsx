7c0729396b8f64d585470b03f221d65ff838b3de
ephraim <ephraims.818@gmail.com>
2026-01-10 17:07:37 +0600

    REACT-RCG - Practice Project - [PROJECT] - FOOD-ORDER-APP  - 03. Adding a Mealitem Component

---
+++ 16. Practice Project - Food Order App/food-order-app/src/Components/MealItem.jsx
@@ -0,0 +1,22 @@
+function MealItem({ meal }) {
+    return (
+        <li className="meal-item">
+            <article>
+                <img
+                    src={`http://localhost:3000/${meal.image}`}
+                    alt={meal.name}
+                />
+                <div>
+                    <h3>{meal.name}</h3>
+                    <p className="meal-tem-price">{meal.price}</p>
+                    <p className="meal-item-price">{meal.description}</p>
+                </div>
+                <p className="meal-item-actions">
+                    <button>Add to Cart</button>
+                </p>
+            </article>
+        </li>
+    );
+}
+
+export default MealItem;
