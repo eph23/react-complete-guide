2155108454e6f311d8425df5911572db6e8615bc
ephraim <ephraims.818@gmail.com>
2026-01-21 04:44:38 +0600

    REACT-RCG - Redux and Modern Redux - [PROJECT] - REDUX-COUNTER - 10. Working With Multiple Slices

--- 17. Redux and Modern Redux/redux-steps/src/store/index.js
+++ 17. Redux and Modern Redux/redux-steps/src/store/index.js
@@ -1,9 +1,9 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 const counterSlice = createSlice({
     name: "counter",
-    initialState: initialState,
+    initialState: initialCounterState,
     reducers: {
         increment(state) {
             state.counter++;
@@ -20,10 +20,28 @@ const counterSlice = createSlice({
     },
 });
 
+const initialAuthState = {
+    isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+    name: "authentication",
+    initialState: initialAuthState,
+    reducers: {
+        login(state) {
+            state.isAuthenticated = true;
+        },
+        logout(state) {
+            state.isAuthenticated = false;
+        },
+    },
+});
+
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 const store = configureStore({
-    reducer: counterSlice.reducer,
+    reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export default store;
