8f626c17e592704f367320ea3ca89e1cabc0db79
ephraim <ephraims.818@gmail.com>
2026-01-24 23:50:03 +0600

    REACT-RCG - Advanced Redux - [PROJECT] - REDUX-CART - 02. Refresher Practice PT-2

--- 18. Redux Deep Dive/redux-cart/src/App.js
+++ 18. Redux Deep Dive/redux-cart/src/App.js
@@ -1,18 +1,18 @@
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
 
-import Cart from "./components/Cart/Cart";
-import Layout from "./components/Layout/Layout";
-import Products from "./components/Shop/Products";
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 function App() {
-    const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
 
-    return (
-        <Layout>
-            {showCart && <Cart />}
-            <Products />
-        </Layout>
-    );
+  return (
+    <Layout>
+      {showCart && <Cart />}
+      <Products />
+    </Layout>
+  );
 }
 
 export default App;
