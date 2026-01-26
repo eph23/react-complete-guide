9bdca493c04fb3af2b760ec7c43913b75bd7bf1c
ephraim <ephraims.818@gmail.com>
2026-01-26 13:11:34 +0600

    REACT-RCG - React Router - 03. Navigating Between Pages

--- 19. React Rounter/food-order-app/src/pages/Products.js
+++ 19. React Rounter/food-order-app/src/pages/Products.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 function ProductsPage() {
-    return <h1>The Products</h1>;
+    return (
+        <>
+            <h1>The Products</h1>
+            <p>
+                Back to <Link to="/">Home Page</Link>
+            </p>
+        </>
+    );
 }
 
 export default ProductsPage;
