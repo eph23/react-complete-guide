4ac02f048df6b0942b6c203c8a563f661678a52c
ephraim <ephraims.818@gmail.com>
2026-01-27 15:15:27 +0600

    REACT-RCG - React Router - 08. Defining and Using Dynamic Routs

--- 19. React Rounter/food-order-app/src/pages/Products.js
+++ 19. React Rounter/food-order-app/src/pages/Products.js
@@ -4,6 +4,13 @@ function ProductsPage() {
     return (
         <>
             <h1>The Products</h1>
+            <ul>
+                <li>Product1</li>
+                <li>Product2</li>
+                <li>Product3</li>
+                <li>Product4</li>
+                <li>Product5</li>
+            </ul>
             <p>
                 Back to <Link to="/">Home Page</Link>
             </p>
